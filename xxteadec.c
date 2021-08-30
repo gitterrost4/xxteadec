9263186c06d5684e17115742524a10e6d6dc1686
Paul Kramer <pk@cantamen.de>
2021-08-30 17:30:50 +0200

    fixes

--- xxteadec.c
+++ xxteadec.c
@@ -18,14 +18,13 @@ int main(int argc, char *argv[]) {
 
     text[fsize] = 0;
     size_t len;
+    char *decrypt_data="";
     //unsigned char *encrypt_data = xxtea_encrypt(text, strlen(text), key, &len);
-    char *decrypt_data = xxtea_decrypt(text, fsize, key, &len);
-    printf("%d\n",fsize);
-    printf("%d\n",strlen(decrypt_data));
-    printf(decrypt_data);
+    decrypt_data = xxtea_decrypt(text, fsize, key, &len);
     //free(encrypt_data);
     FILE *outf = fopen(outFileName, "wb");
     fwrite(decrypt_data,len,1,outf);
+    fclose(outf);
     free(decrypt_data);
     return 0;
 }
